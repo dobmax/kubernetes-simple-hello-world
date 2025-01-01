5d0ae3bf4f50cbeb523da5555debf38f1da79516
Dobrynin Maksym <dobmax90@gmail.com>
2025-01-01 21:03:17 +0100

    Add single-instance mode and scaling
    * Print out node the pod name to a response
    * Add replicated pods manifest

--- src/main/kotlin/com/maksym/dobrynin/kubernetesdemo/HelloWorldController.kt
+++ src/main/kotlin/com/maksym/dobrynin/kubernetesdemo/HelloWorldController.kt
@@ -1,13 +1,18 @@
 package com.maksym.dobrynin.kubernetesdemo
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.server.reactive.ServerHttpRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloWorldController {
 
+    @Value("\${POD_NAME:unknown}")
+    private val podName: String? = null
+
     @GetMapping
-    fun index(): String {
-        return "Hello, world!"
-    }
+    fun index(request: ServerHttpRequest): String =
+        "Hello, world from ${podName}!"
 }
+
